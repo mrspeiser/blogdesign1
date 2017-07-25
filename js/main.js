fad55fbb9d25d2ef43672bb892a63cb9a27aabe3
Matthew Speiser <matthewspeiser@Matthews-MacBook-Pro.local>
2017-07-24 21:58:16 -0700

    first basic layout with some media queries for the header ul

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+  // var page = {
+  //   headertext1 = 'Hey there!',
+  //   headertext2 = 'Welcome to My Blog...',
+  //   headertext3 = 'I hope you find something interesting',
+  //   headertext4 = 'if not',
+  //   headertext5 = 'feel free to email me',
+  //   function slideText() {
+  //
+  //   }
+  // }
+});
