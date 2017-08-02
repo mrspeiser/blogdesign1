5633f161256475cce54e6a029e243870764cabde
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-01 21:29:19 -0700

    working slider with 4 colors, tabs all working with different content. Still needs refining on the specific content and fonts. Make sure if you are adding slides to slider you add 100% to the width for each slide

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 $(function(){
+ 
+  var width = $( window ).width();
+  $('.slide').width(width);
+  var sliderWidth = $('.slider .slide-container').width();
+  var pause = 2000;
+  var animationSpeed = 1000;
+  var currentSlide = 1;
+  
+  var $slider = $('.slider');
+  var $slideContainer = $slider.find('.slide-container');
+  var $slides = $slideContainer.find('.slide');
 
+  setInterval(function(){
+    $slideContainer.animate({'margin-left': "-="+width}, animationSpeed, function(){
+      currentSlide++; 
+        if(currentSlide === $slides.length){
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+        }
+      });
+    
+  }, pause);
 
-  // var page = {
-  //   headertext1 = 'Hey there!',
-  //   headertext2 = 'Welcome to My Blog...',
-  //   headertext3 = 'I hope you find something interesting',
-  //   headertext4 = 'if not',
-  //   headertext5 = 'feel free to email me',
-  //   function slideText() {
-  //
-  //   }
-  // }
 });
